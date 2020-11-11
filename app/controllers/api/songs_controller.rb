41ed339789788431527582863787c2af1f781aea
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-10 20:24:17 -0500

    Created index, show, and create methods. And index.json.jb and show.json.jb

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,2 +1,27 @@
 class Api::SongsController < ApplicationController
+  def index
+    @songs = Song.all
+    render "index.json.jb"
+  end
+
+  def show
+    id = params[:id]
+    @song = Song.find_by(id: id)
+    render "show.json.jb"
+  end
+
+  def create
+    @song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    @song.save
+    render "show.json.jb"
+  end
+
+  def update
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/songs" => "songs#index"
+    get "/songs/:id" => "songs#show"
+    post "/songs" => "songs#create"
+    patch "/songs/:id" => "songs#update"
+  end
 end
